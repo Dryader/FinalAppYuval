2e912fb69720b45a88e7cb19bc47fc23b27d5f43
Yuval <@>
2025-12-11 12:16:41 -0500

    initial

---
+++ FinalAppYuval/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinalAppYuval.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
